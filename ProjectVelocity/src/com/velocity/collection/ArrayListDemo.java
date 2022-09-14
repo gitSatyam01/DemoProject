1460b7cf65e05e48d1c69dc2a0a3c33c20bdff9d
unknown <lilharesatyam@gmail.com>
2022-09-14 16:39:08 +0530

    change to code

--- ProjectVelocity/src/com/velocity/collection/ArrayListDemo.java
+++ ProjectVelocity/src/com/velocity/collection/ArrayListDemo.java
@@ -1,16 +1,15 @@
 package com.velocity.collection;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 public class ArrayListDemo {
 	public static void main(String[] args) {
 		List<String> list = Arrays.asList("satyam","sachin","amol","rahul");
 		
-		Iterator<String> itr = list.iterator();
-		while(itr.hasNext()) {
-			System.out.println(itr.next());
+	
+		for(String str:list) {
+			System.out.println(str);
 		}
 		
 }
