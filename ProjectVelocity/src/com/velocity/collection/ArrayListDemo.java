4c62aec8f8dbcba8d58e4962f68fd455b9a94f9e
unknown <lilharesatyam@gmail.com>
2022-09-14 11:28:17 +0530

    code for ArrayList added

---
+++ ProjectVelocity/src/com/velocity/collection/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.velocity.collection;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("satyam","sachin","amol","rahul");
+		
+		Iterator<String> itr = list.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+		
+}
+}
